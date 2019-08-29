6440d3c67a1a35dc9631b075c0147af5f777dbd5
Bruno Barreira <brunobarreirasilva@gmail.com>
2019-08-29 00:22:34 -0300

    First commit of the project

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,31 @@
+import User from '../models/User';
+
+class UserController {
+  async store(req, res) {
+    const userExists = await User.findOne({ where: { email: req.body.email } });
+
+    if (userExists) {
+      return res
+        .status(400)
+        .json({ message: 'Ops, this email has already been taken!' });
+    }
+
+    const { id, name, email, provider } = await User.create(req.body);
+
+    return res.json({ id, name, email, provider });
+  }
+
+  async update(req, res) {
+    const { email, oldPassword } = req.body;
+
+    const user = await User.findByPk(req.userId);
+
+    if(email !== user.email) {
+
+    }
+
+    return res.json({ ok: true });
+  }
+}
+
+export default new UserController();
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: 'f32fd372fcca3eb84825a9de573b2138',
+  expiresIn: '7d',
+};
