fabedd66fd255f78104c7789724819df83a24ecd
Bruno Barreira <brunobarreirasilva@gmail.com>
2019-08-29 16:31:32 -0300

    Providers being listed and static files being served with express static

--- src/database/index.js
+++ src/database/index.js
@@ -15,7 +15,9 @@ class Databse {
   init() {
     this.connection = new Sequelize(databaseConfig);
 
-    models.map(model => model.init(this.connection));
+    models
+      .map(model => model.init(this.connection))
+      .map(model => model.associate && model.associate(this.connection.models));
   }
 }
 
