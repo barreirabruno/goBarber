c74533d9a0587e622dbde63e154b0ee9465e18fb
Bruno Barreira <brunobarreirasilva@gmail.com>
2019-08-29 22:57:50 -0300

    Nodemailer and mailtrap to send email configs

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '250dcb512e89a4',
+    pass: 'aa203c023a2c09',
+  },
+  default: {
+    from: 'Equipe Go Barber <noreply@gobarber.com>',
+  },
+};
